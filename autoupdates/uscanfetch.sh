b0258d02ebbd955a4277b4eed8b5a76e7670ba10
Saubhik Datta <saubhikdatta@gmail.com>
2025-05-21 17:13:00 +0200

    Edit watch files

--- autoupdates/uscanfetch.sh
+++ autoupdates/uscanfetch.sh
@@ -13,16 +13,16 @@ mkdir -p /tmp/work/$DIR/debian
 cd /tmp/work/$DIR
 CAP_USCAN='capture_uscan'
 touch $CAP_USCAN
-echo "Changing to Directory :" /tmp/work/$DIR
+echo "##Changing to Directory :" /tmp/work/$DIR
 
-echo "Package Name determined: " $REPONAME
+echo "##Package Name determined: " $REPONAME
 REPOURL="https://github.com/gardenlinux/${REPONAME}"
 git clone $REPOURL
 cd $REPONAME
-echo "Changing to Directory :" $REPONAME
-
-GETVER=$(grep version= $PRPSRC | cut -d'=' -f2 | cut -d'"' -f2 | cut -d'-' -f1)
-GETVER_ORIG=$(grep version_orig= $PRPSRC | cut -d'=' -f2 | cut -d'"' -f2 | cut -d'-' -f1 )
+echo "##Changing to Directory :" $REPONAME
+echo ----
+GETVER=$(grep version= $PRPSRC | cut -d'=' -f2 | cut -d'"' -f2 )
+GETVER_ORIG=$(grep version_orig= $PRPSRC | cut -d'=' -f2 | cut -d'"' -f2 )
 if [[ "$1" =~ "python" ]]; then 
 	GITBRVER=$(grep git_src $PRPSRC | grep github | awk '{print $3}' | sed 's#.*/##; s/^v//')
 else
@@ -41,15 +41,25 @@ elif [[ -n "${GITBRVER}" && "${GITBRVER}" =~ [0-9]+$ ]]; then
     VER="${GITBRVER}"
 fi
 
-echo "Version determined : " $VER
+echo "##Version determined : " $VER
+echo ----
 echo "$PKGNAME (${VER}) unstable; urgency=medium" > /tmp/work/$DIR/debian/changelog
 uscan --report --watch $HOME/watch_$PKGNAME | tee $CAP_USCAN 
 NEWVER=$(grep "Newest version" $CAP_USCAN | cut -d',' -f1 | rev | cut -d' ' -f1 | rev)
-sed -i "s|${VER}|${NEWVER}|g" $PRPSRC
+echo ----
+echo "##New Version determined: " $NEWVER
+if [[ "${NEWVER}" =~ [0-9]+$  ]]; then
+	sed -i "s|${VER}|${NEWVER}|g" $PRPSRC
+	echo ==========================================================
+	echo "prepare_source : "
+	echo '##############'
+	cat $PRPSRC
+	echo ==========================================================
+else
+	echo ----
+	echo "IMPORTANT: Could not retrieve the Newer Version information using uscan for this package"
+	exit 3
+fi
 
-echo ==========================================================
-echo "prepare_source : "
-echo '##############'
-cat $PRPSRC
-echo ==========================================================
 #rm -rf /tmp/work/$DIR
+:>$CAP_USCAN
